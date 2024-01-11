328a235d78c8d249866cae27b11065e586f6cbad
stp2003 <100586951+stp2003@users.noreply.github.com>
2024-01-11 10:32:19 +0530

    custom button made

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final Function() onTap;
+  final Color color;
+
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onTap,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color,
+          shape: const StadiumBorder(),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+}
